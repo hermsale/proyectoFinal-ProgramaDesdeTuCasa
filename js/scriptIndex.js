a978be4ebc7c785bbb3e99cf3583b141a49c88c1
hermsale <hermsale@gmail.com>
2020-11-11 14:47:26 -0300

    actualizacion index
    crea usuario por index

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -2,29 +2,65 @@
 // BIENVENIDO A PROBA ASIA
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// cuando se carga la pagina muestra los productos 
+let usuarioArray = [];
 
-let primeraVez = 0;
+
 window.addEventListener("load",()=>{
     let existentes = JSON.parse(localStorage.getItem("usuario"));
 
     if(existentes===null){
-        alert("Bienvenido por primera vez!");
-        alert("En esta primera carga, deberás crear tu usuario");
-        alert("Lo que coloques a continuación será tu usuario y contraseña de aqui en más");
-        // existentes.push('usuario');
-       primeraVez = 1;
+        primeraCarga();
     }
+
 });
 
-let nombre = document.querySelector("#nombre")
+let nombre = document.querySelector("#nombre");
+let contraseña = document.querySelector("#contraseña");
+let btn_acceder = document.querySelector("#btn_acceder");
 
 
 
 function primeraCarga(){
-    // if(primeraVez==='1'){
-        localStorage.setItem('usuario',"ale");
-        console.log("creaste usuario");
-    // }
+        alert("Bienvenido por primera vez!");
+        alert("En esta primera carga, deberás crear tu usuario");
+        alert("Lo que coloques a continuación será tu usuario y contraseña de aqui en más");
+}
+
+btn_acceder.addEventListener("click",tomarDatos);
+
+
+
+function tomarDatos(){
+    let usuarioNombre = nombre.value;
+    let usuarioPassword = contraseña.value;
     
+    // console.log(usuarioNombre + usuarioPassword);
+    guardarUsuario(usuarioNombre,usuarioPassword);
+}
+
+function guardarUsuario(usuarioNombre,usuarioPassword){
+    let existentes =  JSON.parse(localStorage.getItem("usuario"));
+
+    const usuario ={
+        nombreUsuario: usuarioNombre,
+        contraseñaUsuario: usuarioPassword
+    }
+
+    if(existentes!==null){
+        existentes.push(usuario);
+        localStorage.setItem('usuario',JSON.stringify(existentes));
+    }else{
+        usuarioArray.push(usuario);
+        localStorage.setItem('usuario',JSON.stringify(usuarioArray));
+    }
+
+    limpiarForm()
+
+}
+
+
+// limpiar formulario de ingreso
+function limpiarForm(){
+    usuarioNombre.value = "";
+    usuarioPassword.value = "";
 }
