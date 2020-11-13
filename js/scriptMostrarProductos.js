3255604cd11db86bd8294d33689034a4ab2534ed
hermsale <hermsale@gmail.com>
2020-11-13 18:04:52 -0300

    eliminar item
    se agrego funcionalidad al boton eliminar item.
    se corrigieron bugs

--- js/scriptMostrarProductos.js
+++ js/scriptMostrarProductos.js
@@ -6,6 +6,7 @@
 // cuando se carga la pagina muestra los productos 
 window.addEventListener("load",()=>{
         mostrarProductos();
+
 });
 
 let btn_borrarTodo = document.querySelector("#btn_borrarTodo");
@@ -18,7 +19,7 @@ let contenedor_borrar = document.querySelector("#contenedorBorrar");
 btn_borrarTodo.addEventListener("click",borrarProducto);
 
 
-// borra un producto 
+// borra todo
 function borrarProducto(){
     let decision = confirm("Seguro que desea borrar todo?");
         if(decision==true)localStorage.removeItem('producto');   
@@ -32,8 +33,10 @@ function mostrarProductos(){
     // console.log("entre");
     let productosCargados = JSON.parse(localStorage.getItem("producto"));
 
+
     // se fija si hay algo para mostrar, si no hay nada para mostrar oculta el boton y el contenedor de borrar todo 
     if(productosCargados===null){
+        
         btn_borrarTodo.classList.toggle("borrar");
         contenedor_borrar.classList.toggle("borrar");
     }
@@ -42,116 +45,64 @@ function mostrarProductos(){
     for (const index in productosCargados){
         span_mostrarProducto.innerHTML += `<br><span class="itemProducto">${productosCargados[index].nombreProducto}</span>`;
         span_mostrarCantidad.innerHTML += `<br><span class="itemProducto">${productosCargados[index].cantidad}</span>`;
-        span_precioProducto.innerHTML += `<br><span class="itemProducto">${productosCargados[index].precio}</span> <button id="btn_borrarItem" class="eliminarItem"> eliminar </button>`;            
-    }
+        span_precioProducto.innerHTML += `<br><span class="itemProducto" >${productosCargados[index].precio}</span> <button id="btn_borrarItem" class="eliminarItem" onclick="buscador(${index})"> eliminar </button>`;            
+    }                                               // le paso el parametro de la posicion de index a la funcion 
 
-    // productosCargados.forEach(index => {
-    //     span_mostrarProducto.innerHTML += `<br><span>${productosCargados[index].nombreProducto}</span>`;
-    //     span_mostrarCantidad.innerHTML += `<br><span>${productosCargados[index].cantidad}</span>`;
-    //     span_precioProducto.innerHTML += `<br><span>${productosCargados[index].precio}</span>`;            
-    // });
-
-    // console.log(productosCargados.nombreProducto);
 
 }   
 
-///////////////////////////////////////////////////////////////////////
-
-// NOTAS: Revisar metodo indexOf y splice para eliminar items
-// el metodo indexOf devuelve el indice del array de donde se encuentre la coincidencia.
-// //El método splice() cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
-// const months = ['Jan', 'March', 'April', 'June'];
-// months.splice(2,1);
-// de esta forma splice borraria el indice 2, 1 solo elemento
-
-
-
-// NOTA: Funcion a desarrollar
-// funcion para buscar la posicion del item a eliminar
-
-// let eliminarItem = document.querySelector(".eliminarItem");
-// eliminarItem.addEventListener("click",function(){
-    
-// }
-
-// function eliminar(){
-//     // let eliminarItem = document.querySelector(".eliminarItem");
-//     eliminarItem.addEventListener("click",buscador());
-// }
-// arrayBI.splice(i,1);
-//                 //COMO PRUEBA, subo arrayBI como "pruebaListadoProductos". Si arranca, luego intentar subirlo reemplazando la array original "listadoDeProductos";
-//                 localStorage.setItem("listadoDeProductos", JSON.stringify(arrayBI));
-//             }
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// eliminarItem = document.querySelectorAll(".eliminarItem");
 
-btn_borrarItem = document.querySelectorAll("#btn_borrarItem");
-
-// btn_borrarItem.addEventListener("click",buscador);
-// btn_borrarTodo.addEventListener("click",borrarProducto);
-
-//     let productosCargados = JSON.parse(localStorage.getItem("producto"));
-//     for (const index in productosCargados){
-//         productosCargados[index].nombreProducto;
-//     }
-// })
-
-btn_borrarItem.addEventListener("click",buscador);
+    // eliminar item 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // esta funcion buscador, busca el elemento a eliminar, dependiendo el boton de eliminar qeu se pulse 
-function buscador(){
-  
-    // for(let i = 0; i<eliminarItem.lenght; i++){
-    //     console.log("valores cargados "+eliminarItem[i]);
-    // }    
+// recibe el parametro de la ubicacion que se pulso 
+function buscador(index){
 
+    // compruebo el indice que se pulso en la lista 
+    console.log(index);
+
+    // traigo el objeto json 
     let buscar = JSON.parse(localStorage.getItem("producto"));
-    let nombreBusqueda = "manteca";
-    for (const i in buscar) {
-       
-        if(buscar[i].nombreProducto.indexOf(nombreBusqueda)===0){
-            console.log("lo encontre "+i);
 
-            buscar.splice(i,1);
-            localStorage.setItem('producto',JSON.stringify(buscar));
-            location.reload();
-          
+    // busco el producto por su indice en la lista 
+    let nombreBusqueda = buscar[index].nombreProducto;
+
+  
+        // localStorage.removeItem('producto');
+ 
+        for (const i in buscar) {
+           
+            // si encuentra el producto entra en el if 
+    
+            
+            if(buscar[i].nombreProducto.indexOf(nombreBusqueda)===0){
+                console.log("lo encontre "+i);
+    
+                // quita el valor 
+                buscar.splice(i,1);
+                
+                // vuelve a cagar el array nuevo 
+                localStorage.setItem('producto',JSON.stringify(buscar));
+
+                // si se encuentra en la posicion 0 significa que despues no hay nada mas por ende, borra la key producto 
+                if(i==='0'){
+                    localStorage.removeItem('producto');
+                }
+                
+                // refresca la pagina para ver los cambios
+                location.reload();
+    
+            }
         }
     }
-}
 
 
-// codigo profesora 
+// function ultimoItemBorrar(){
+//     let ultimoItem = JSON.parse(localStorage.getItem("producto"));
 
-// function eliminarItem(e){
-//     e.preventDefault();
-//     let productos = localStorage.getItem('productos');
-//     //Parseamos nuestro array de string a un JSON 
-//     let productosObj = JSON.parse(productos)
-
-//     //esto atrapa el click que estamos haciendo en una de las tablas especificas
-//     //lo coloco en consola para que puedan ver que está obteniendo
-//     console.log(e.path[2].cells[0].innerHTML);
-//     //aqui pueden ver la ruta de las propiedades que estoy consultando arriba
-//     // console.log(e);
-//     //Con este for recorre el array que contiene todos los objetos de los productos para luego intentar comparar los datos
-//     //obtenidos en el click, me enfoco en lo unico que es UNICO, osea el id
-//     for(let i =0; i< productosObj.length;i++){
-     
-//         for(let objectProp in productosObj[i]){
-//             // recorro las propiedades de cada uno de los objetos en el array. Para poder comparar el contenido de la fila que capture en el click y saber qué borrar
-//             if(parseInt(e.path[2].cells[0].innerHTML) === productosObj[i][objectProp]){
-//                 // elimino el objeto del array productosObj --> splice elimina en su primer parametro el indec que indicamos y en el segundo parametro la cantidad de datos que queremos eliminar a partir de ese indice
-//                 productosObj.splice(i,1);
-//                 //Volvemos a almacenar la lista en el localStorage, pero con un producto/item menos
-//                 localStorage.setItem('productos', JSON.stringify(productosObj))
-//                //eliminamos la fila del DOM
-//                 e.path[2].innerHTML = "";
-//             }
-//         }
-        
-//     }
-//     // mostramos los productos de vuelta en la tabla
-//     mostrarProductos();
+//     console.log(ultimoItem[0].nombreProducto);
+//     // if(ultimoItem.nombreProducto)
+//     // localStorage.removeItem('producto');
 // }
